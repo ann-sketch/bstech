17e584121e39035609dd886e5ed20a1e97f31b4b
ann-sketch <61893160+ann-sketch@users.noreply.github.com>
2021-01-14 23:19:36 +0000

    Update urls.py

--- bstech/urls.py
+++ bstech/urls.py
@@ -1,11 +1,19 @@
-from django.urls import include, path
-from rest_framework import routers
-from . import views
-
-router = routers.DefaultRouter()
-router.register(r'accounts', views.AccountViewSet)
+from django.contrib import admin
+from django.urls import path,  include
+from app.views import (
+    home_view,
+    safoa_view,
+    condom_view,
+    eye_view,
+    signup_view,
+)
 
 urlpatterns = [
-    path('', include(router.urls)),
-    path('api-auth/', include('rest_framework.urls', namespace='rest_framework'))
+    path('', home_view, name='home'),
+    path('safoa/', safoa_view, name='safoa'),
+    path('condom/', condom_view, name='condom'),
+    path('eye/', eye_view, name='eye'),
+    path('register/', signup_view, name='register'),
+    path('', include('app.urls')),
+    path('admin/', admin.site.urls),
 ]
